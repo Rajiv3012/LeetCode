3137058adee3f35010e48cb56dc732869a77e111
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2025-11-03 23:10:32 +0530

    Time: 0 ms (100%), Space: 43 MB (8.69%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,16 +10,27 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int count=0;
-        ListNode temp = head;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
+        // int count=0;
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     count++;
+        //     temp=temp.next;
+        // }
+        // temp= head ;// move it back           // this is done by using count
+        // for(int i=0;i<count/2;i++){
+        //     temp=temp.next;
+        // }
+        // return temp;
+
+
+
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){      //turtoise and hare method one runs slow
+            fast= fast.next.next;                  // and one runs fast
+            slow = slow.next;
         }
-        temp= head ;// move it back
-        for(int i=0;i<count/2;i++){
-            temp=temp.next;
-        }
-        return temp;
+        return slow;
     }
 }
