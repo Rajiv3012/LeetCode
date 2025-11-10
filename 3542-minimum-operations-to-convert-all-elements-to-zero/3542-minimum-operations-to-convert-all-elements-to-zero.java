702963fe31de38951a31890cbd363b47c90a902f
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2025-11-10 23:48:35 +0530

    Time: 12 ms (96.48%), Space: 136.8 MB (5.63%) - LeetHub

--- 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int n = nums.length;
-        int ops = 0;
-
-        for (int i = 0; i < n; i++) {
-            // skip zeros
-            if (nums[i] == 0) continue;
-
-            ops++; // one new operation
-            int min = nums[i];
-
-            // reduce subarray till the next zero region
-            for (int j = i; j < n && nums[j] > 0; j++) {
-                nums[j] -= min;
+        var stack = new int[nums.length + 1];
+        var top = 0;
+        var ans = 0;
+        for (var i = 0; i < nums.length; i++) {
+            while (stack[top] > nums[i]) {
+                top--;
+                ans++;
             }
+            if (stack[top] != nums[i])
+                stack[++top] = nums[i];
         }
-        return ops;
+        return ans + top;
     }
 }
