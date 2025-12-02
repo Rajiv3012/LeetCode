a1307b47a6351f259c2ce4aa8cb80183d1d82297
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2025-12-02 21:32:30 +0530

    Update 0061-rotate-list.java

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,33 +1,75 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.Scanner;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
 class Solution {
+
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null){
-            return head;
-        }
+        if (head == null || head.next == null) return head;
+
+        // Find length + tail
         ListNode tail = head;
-        int count=1;
-        while(tail.next!=null){
-            tail=tail.next;
+        int count = 1;
+        while (tail.next != null) {
+            tail = tail.next;
             count++;
         }
-        int position =k%count;
-        if(position==0)return head;
-        ListNode temp= head;
-        for(int i=0;i<count-position-1;i++){
-            temp=temp.next;
+
+        // Reduce k
+        int pos = k % count;
+        if (pos == 0) return head;
+
+        // Move to new tail
+        ListNode temp = head;
+        for (int i = 0; i < count - pos - 1; i++) {
+            temp = temp.next;
         }
-        ListNode newhead = temp.next;
-        temp.next=null;
-        tail.next=head;
-        return newhead;
+
+        // Rotate
+        ListNode newHead = temp.next;
+        temp.next = null;
+        tail.next = head;
+
+        return newHead;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take size
+        int n = sc.nextInt();
+
+        // Build list
+        ListNode head = null, curr = null;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (head == null) {
+                head = new ListNode(x);
+                curr = head;
+            } else {
+                curr.next = new ListNode(x);
+                curr = curr.next;
+            }
+        }
+
+        // Take k
+        int k = sc.nextInt();
+
+        // Rotate
+        Solution s = new Solution();
+        ListNode ans = s.rotateRight(head, k);
+
+        // Print result
+        while (ans != null) {
+            System.out.print(ans.val + " ");
+            ans = ans.next;
+        }
     }
 }
