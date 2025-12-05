ceb84470e7b8e1c27b0c94aabac2adf69e5acfac
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2025-12-05 21:16:16 +0530

    Update prime-factors.java

--- Difficulty: Easy/Prime Factors/prime-factors.java
+++ Difficulty: Easy/Prime Factors/prime-factors.java
@@ -1,27 +1,47 @@
 class Solution {
+
     public static ArrayList<Integer> primeFac(int n) {
-        // code here
         ArrayList<Integer> ans = new ArrayList<>();
         int x = (int)Math.sqrt(n);
-        if(n%2==0){
+
+        // factor 2
+        if (n % 2 == 0) {
             ans.add(2);
-            while(n%2==0){
-                n=n/2;
+            while (n % 2 == 0) {
+                n = n / 2;
             }
-         x = (int)Math.sqrt(n);//baar baar n update hora
+            x = (int)Math.sqrt(n); // updating because n changed
         }
-        for(int i=3;i<=x;i=i+2){
-            if(n%i==0){
+
+        // odd factors
+        for (int i = 3; i <= x; i += 2) {
+            if (n % i == 0) {
                 ans.add(i);
             }
-            while(n%i==0){
-                n=n/i;
+            while (n % i == 0) {
+                n = n / i;
             }
-             x = (int)Math.sqrt(n);
+            x = (int)Math.sqrt(n); // update again because n changed
         }
-        if(n>1){
+
+        // leftover prime
+        if (n > 1) {
             ans.add(n);
         }
+
         return ans;
     }
+
+    /*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<Integer> result = primeFac(n);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+    */
 }
