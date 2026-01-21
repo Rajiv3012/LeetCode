108af4cd1de1f633160201684cc704f3cb891c3e
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-01-21 23:05:51 +0530

    Time: 33 ms (33.56%), Space: 156.2 MB (13.48%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,46 @@
+import java.util.*;
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length;
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
         Deque<Integer> dq = new LinkedList<>();
-        int [] result = new int[n-k+1];
-        for(int i=0;i<n;i++){
-            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
+
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
                 dq.pollFirst();
             }
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
                 dq.pollLast();
             }
             dq.offerLast(i);
-            if(i>=k-1){
-                result[i-k+1] = nums[dq.peekFirst()];
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[dq.peekFirst()];
             }
         }
-        return result;    
+        return result;
     }
 }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] nums = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = sc.nextInt();
+//         }
+//         int k = sc.nextInt();
 
+//         Solution sol = new Solution();
+//         int[] ans = sol.maxSlidingWindow(nums, k);
+
+//         for (int val : ans) {
+//             System.out.print(val + " ");
+//         }
+
+//         sc.close();
+//     }
+// }
 //brute force
 // class Solution {
 //     public int[] maxSlidingWindow(int[] nums, int k) {
