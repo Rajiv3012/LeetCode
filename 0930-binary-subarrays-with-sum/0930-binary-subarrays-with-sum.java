0425f7206f0889a771ea64d5f4c138a4f695d7cc
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-02-01 19:16:48 +0530

    Time: 1 ms (99.76%), Space: 51.3 MB (25.86%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,40 +1,23 @@
-import java.util.*;
-
 class Solution {
-
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atmost(nums, goal) - atmost(nums, goal - 1);
+        return atmost(nums,goal)- atmost(nums,goal-1);
     }
-
-    private int atmost(int[] nums, int goal) {
-        if (goal < 0) return 0;
-
-        int l = 0, r = 0;
-        int sum = 0, count = 0;
-
-        while (r < nums.length) {
-            sum += nums[r];
-
-            while (sum > goal) {
-                sum -= nums[l];
-                l++;
+    private int atmost(int [] nums,int goal){
+        int sum=0;
+        int n = nums.length;
+        int left=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(goal<0){
+                return 0;
             }
-
-            count += (r - l + 1);
-            r++;
+            sum+=nums[i];
+            while(sum>goal){
+                sum-=nums[left];
+                left++;
+            }
+            count+=(i-left+1);
         }
         return count;
-    }
-
-    // 🔹 Main method
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        // Example input
-        int[] nums = {1, 0, 1, 0, 1};
-        int goal = 2;
-
-        int result = sol.numSubarraysWithSum(nums, goal);
-        System.out.println("Number of subarrays with sum = " + goal + " : " + result);
     }
 }
