778e2325ca1bcdf93ffd5a13faf8b9afd9f78b91
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-02-06 22:50:02 +0530

    Time: 12 ms (44.45%), Space: 47.8 MB (36.42%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,13 +1,12 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        Integer arr[] = new Integer [n];
-
+        Integer [] arr = new Integer[n];
         for(int i=0;i<n;i++){
             arr[i]=i;
         }
         Arrays.sort(arr,(a,b)-> score[b]-score[a]);
-        String[] ans = new String[n];
+        String [] ans = new String[n];
         for(int i=0;i<n;i++){
             if(i==0){
                 ans[arr[i]]="Gold Medal";
@@ -16,7 +15,7 @@ class Solution {
             }else if(i==2){
                 ans[arr[i]]="Bronze Medal";
             }else{
-                ans[arr[i]] = "" + (i + 1);
+                ans[arr[i]]= ""+(i+1);
             }
         }
         return ans;
