ac515940847899e0b2da33294295bb6bbf66545a
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-02-16 23:24:54 +0530

    Time: 23 ms (8.65%), Space: 52.2 MB (98.76%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        int res = nums[0];
+        for(int i=0;i<n;i++){
+            int freq= map.getOrDefault(nums[i],0)+1;
+            map.put(nums[i],freq);
+            if(freq>map.getOrDefault(res,0)){
+                res = nums[i];
+            }
+        }
+        return res;
+    }
+}
