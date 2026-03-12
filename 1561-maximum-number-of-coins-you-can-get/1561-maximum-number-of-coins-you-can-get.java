20eec0e0b501c227f906f3d61a7cb784263c6cb2
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-03-12 14:31:22 +0530

    Refactor maxCoins method and add main method

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,11 +1,34 @@
-class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
     public int maxCoins(int[] piles) {
-        int n = piles.length;
         Arrays.sort(piles);
-        int total=0;
-        for(int i=n/3;i<n;i+=2){
-            total+=piles[i];
+        int n = piles.length;
+        int total = 0;
+
+        for (int i = n / 3; i < n; i += 2) {
+            total += piles[i];
         }
+
         return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Number of piles: ");
+        int n = sc.nextInt();
+
+        int[] piles = new int[n];
+        System.out.print("Enter piles: ");
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
+        }
+
+        Solution sol = new Solution();
+        System.out.println("Max coins: " + sol.maxCoins(piles));
+
+        sc.close();
     }
 }
