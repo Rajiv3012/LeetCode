5e8302f4ffa2868fa8d086e9dd7dad1d8f50aa08
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-03-22 20:05:04 +0530

    Time: 0 ms (100%), Space: 47 MB (34.19%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,28 +13,46 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> wraplist = new ArrayList<>();
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         if(root==null){
+//             return wraplist;
+//         }
+//         queue.offer(root);
+//         while(!queue.isEmpty()){
+//         int levelnum = queue.size();
+//         List<Integer>sublist = new ArrayList<>();
+//         for(int i=0;i<levelnum;i++){
+//             if(queue.peek().left!=null){
+//                 queue.offer(queue.peek().left);
+//             }
+//             if(queue.peek().right!=null){
+//                 queue.offer(queue.peek().right);
+//             }
+//             sublist.add(queue.poll().val);
+//         }
+//         wraplist.add(sublist);
+//         }
+//         return wraplist;
+//     }
+// }
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> wraplist = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> ans = new ArrayList<>();
+        lvl(root,0,ans);
+        return ans;
+    }
+    public void lvl(TreeNode root, int level,List<List<Integer>> ans ){
         if(root==null){
-            return wraplist;
+            return;
         }
-        queue.offer(root);
-        while(!queue.isEmpty()){
-        int levelnum = queue.size();
-        List<Integer>sublist = new ArrayList<>();
-        for(int i=0;i<levelnum;i++){
-            if(queue.peek().left!=null){
-                queue.offer(queue.peek().left);
-            }
-            if(queue.peek().right!=null){
-                queue.offer(queue.peek().right);
-            }
-            sublist.add(queue.poll().val);
+        if(level == ans.size()){
+            ans.add(new ArrayList<>());
         }
-        wraplist.add(sublist);
-        }
-        return wraplist;
+        ans.get(level).add(root.val);
+        lvl(root.left,level+1,ans);
+        lvl(root.right,level+1,ans);
     }
 }
