83439b46dec70ceee960994c9f030d6f90c28858
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-03-22 20:41:55 +0530

    Time: 1 ms (96.5%), Space: 44.5 MB (51.69%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -15,20 +15,47 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        lvl(root,0,ans);
+        if(root==null){
+            return ans;
+        }
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int lvl = queue.size();
+            List<Integer> subans = new ArrayList<>();
+            for(int i=0;i<lvl;i++){
+                if(queue.peek().left!=null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right!=null){
+                    queue.offer(queue.peek().right);
+                }
+                subans.add(queue.poll().val);
+            }
+            ans.add(subans);
+        }
         Collections.reverse(ans);
         return ans;
     }
-    public void lvl(TreeNode root, int level,List<List<Integer>> ans ){
-        if(root==null){
-            return;
-        }
-        if(level== ans.size()){
-            ans.add(new ArrayList<>());
-        }
-        ans.get(level).add(root.val);
-        lvl(root.left,level+1,ans);
-        lvl(root.right,level+1,ans);
-    }
 }
+
+// class Solution {
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         lvl(root,0,ans);
+//         Collections.reverse(ans);
+//         return ans;
+//     }
+//     public void lvl(TreeNode root, int level,List<List<Integer>> ans ){
+//         if(root==null){
+//             return;
+//         }
+//         if(level== ans.size()){
+//             ans.add(new ArrayList<>());
+//         }
+//         ans.get(level).add(root.val);
+//         lvl(root.left,level+1,ans);
+//         lvl(root.right,level+1,ans);
+//     }
+// }
