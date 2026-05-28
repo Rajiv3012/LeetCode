d358073e9abf371d066d8e5e55fffb8396c8f8ac
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-05-28 23:28:07 +0530

    Time: 2 ms (73.38%), Space: 44.2 MB (19.17%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int maxi=0;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            int a = entry.getValue();
+            maxi= Math.max(maxi,a);
+        }
+        int count=0;
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()==maxi){
+                count++;
+            }
+        }
+        return maxi*count;
+    }
+}
