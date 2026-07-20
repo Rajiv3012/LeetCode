acd663bc5da08fdad799a48def23f942cda7f554
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-07-20 11:32:02 +0530

    Time: 9 ms (100%), Space: 172.9 MB (100%) - LeetHub

---
+++ 3997-count-dominant-nodes-in-a-binary-tree/3997-count-dominant-nodes-in-a-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans=0;
+    public int countDominantNodes(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    public int dfs(TreeNode root){
+        if(root==null){
+            return Integer.MIN_VALUE;
+        }
+        int leftmax= dfs(root.left);
+        int rightmax = dfs(root.right);
+        int currmaxx = Math.max(root.val, Math.max(leftmax, rightmax));
+        if(currmaxx==root.val){
+            ans++;
+        }
+        return currmaxx;
+    }
+}
