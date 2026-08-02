9bdcb0f76f7d52e3d9a961012abcb7ca4cab761d
Rajiv Kumar Singh <152844193+Rajiv3012@users.noreply.github.com>
2026-08-02 21:17:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,39 +1,23 @@
-import java.util.*;
-
-public class Solution {
-
-    public static int[] nextGreaterElements(int[] nums) {
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
-        int[] ans = new int[n];
-
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
+        int []ans = new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
                 st.pop();
             }
-            if (i < n) {
-                ans[i] = st.isEmpty() ? -1 : st.peek();
+            if(st.isEmpty()){
+                map.put(nums[i],-1);
+            }else{
+                map.put(nums[i],nums[st.peek()]);
             }
-            st.push(nums[i % n]);
+            st.push(i);
+        }
+        for(int i=0;i<n;i++){
+            ans[i]=map.get(nums[i]);
         }
         return ans;
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        int[] result = nextGreaterElements(nums);
-
-        for (int x : result) {
-            System.out.print(x + " ");
-        }
-    }
 }
-
